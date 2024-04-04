a659149ba5e2906864449502fec0360902b9e99e
Mario Lazzari <mario.lazzari@gmail.com>
2024-04-04 22:32:07 +0200

    Done lesson 47

--- test/data/queues.test.ts
+++ test/data/queues.test.ts
@@ -1,10 +1,9 @@
-import { Queue } from "data/queues";
+import { CircularQueue, Queue } from "data/queues";
 import { describe, expect, it } from "vitest";
 
 describe("Queues", () => {
   it("should return empty queue", () => {
     const queue = new Queue<number>();
-
     expect(queue.isEmpty()).toBeTruthy();
   });
 
@@ -25,6 +24,24 @@ describe("Queues", () => {
     const item = queue.dequeue();
 
     expect(queue.size()).toBe(2);
+    expect(item).toBe(10);
+  });
+});
+
+describe("Circular Queues", () => {
+  it("should return empty queue", () => {
+    const cq = new CircularQueue<number>(5);
+    expect(cq.isEmpty()).toBeTruthy();
+  });
+
+  it("should dequeue 1 element1", () => {
+    const cq = new CircularQueue<number>(5);
+    cq.enqueue(10);
+    cq.enqueue(20);
+    cq.enqueue(30);
+    const item = cq.dequeue();
+    console.log(item);
+
     expect(item).toBe(10);
   });
 });
